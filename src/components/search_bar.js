1342192b6986931199d7d91db1972d6c2823901a
lizparody <lizparody@gmail.com>
2017-09-15 10:20:57 -0500

    updated the state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'; //even if we don't use it here we still need to import it
 
 class SearchBar extends Component { //new class called SearchBar and give it access to all the functionality that React.Component has
+  constructor(props) { //is the first and only function called automatically whenever a new instance f the class is created
+    super(props); //calling the parent method Component
+
+    this.state = { term: '' };
+  }
+
   render() { //Every class component needs to have a defined render method
-    return <input onChange={event => console.log(event.target.value)}/>;
-  } //onChange is the name of the event, this.onInputChange is the reference to the event handler
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })}/>
+      </div>
+    );
+  }
 
 }
 export default SearchBar;
