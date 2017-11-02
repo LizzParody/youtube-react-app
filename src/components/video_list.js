1174eab63b087e3a8e672df4834dbd9bab5f7dd6
lizparody <lizparody@gmail.com>
2017-11-01 19:05:27 -0500

    made it more clear

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,12 +2,12 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const videoItems = props.videos.map((video) => { //return an array of videos
+  const videoItems = props.videos.map((video1) => { //return an array of videos
     return (
       <VideoListItem
       onVideoSelect={props.onVideoSelect} //takes the property from App and pass it to VideoListItem
-      key={video.etag} // identifier of each video so when you want to change one specific video you can do it
-      video={video} /> //passing this as a property of VideoListItem
+      key={video1.etag} // identifier of each video so when you want to change one specific video you can do it
+      video={video1} /> //passing this as a property of VideoListItem
     )
   });
 
